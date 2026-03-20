c65220914867baca6e9b3c448c2e08d12af172f3
gudtldn <gudtldn1234@gmail.com>
2026-03-20 14:25:25 +0900

    splicer test 코드 작성

---
+++ tests/splicer_test.cpp
@@ -0,0 +1,17 @@
+#include <meta>
+#include <print>
+
+int main()
+{
+    auto a = 1;
+    constexpr auto a_info = ^^decltype(a);
+
+    typename[:a_info:] b = 2;
+    std::println("a + b = {}", a + b);
+
+    std::println(
+        "a type: {}, b type: {}",
+        std::meta::display_string_of(a_info),
+        std::meta::display_string_of(^^decltype(b))
+    );
+}
